d182e224a5c74fe7791919cd835ee34caa9920e2
dpuskuri <64773180+dpuskuri@users.noreply.github.com>
2024-06-17 20:37:53 +0000

    updated files

--- lists-using-functions.py
+++ lists-using-functions.py
@@ -1,1 +1,27 @@
-deee
+import os
+
+def list_files_in_folder(folder_path):
+
+    try:
+        files = os.listdir(folder_path)
+        return files, None
+    except FileNotFoundError:
+        return None, "file not found"
+    except PermissionError:
+        return None, "permission denied"
+
+def main():
+    folder_paths = input("provie the folder name saperated by spaces to list files:").split()
+
+    for folder_path in folder_paths:
+        files, error_message = list_files_in_folder(folder_path)
+        if files:
+            print(f"Files in {folder_path}:")
+            for file in files:
+                print(file)
+        else:
+            print(f"error in {folder_path}: {error_message}")
+
+if __name__ == "__main__":
+    main()
+
